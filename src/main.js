f205dd2b421426b1ebb5e8332392197393ba276e
Stine Julius <stine.insecure@protonmail.com>
2025-05-22 16:30:24 +0200

    Setup scss and modern-normalize

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import 'modern-normalize';
+import './style/main.scss';
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,14 @@
 
 <template>
   <main>
-    
+    <h1>Hello</h1>
   </main>
 </template>
+
+<style lang="scss" scoped>
+@use '@/style' as *;
+
+h1 {
+  color: $red;
+}
+</style>
